f9720e081d226d8bedb444782b241cc1e59b0fac
Max Anisimov <makc.anisimov777@yandex.ru>
2024-01-01 22:55:25 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import Phaser from "phaser";
+import './style.css'
+
+new Phaser.Game({
+  width: 800,
+  height: 600,
+  title: 'My Test Phaser RPG',
+  url: import.meta.env.URL || '',
+  version: import.meta.env.VERSION || '0.0.1 ',
+  backgroundColor: '#000',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  pixelArt: true,
+});
